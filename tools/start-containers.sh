5305f22a96c69c0275dfb3fe50f2d215c6947078
Paul Steele <paul@paul-steele.com>
2021-11-07 15:24:30 -0500

    update start containers

--- tools/start-containers.sh
+++ tools/start-containers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-podman run -d --rm --name hub-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass mysql --default-authentication-plugin=mysql_native_password 
-podman run -d --rm --name hub-adminer -p 5200:8080 adminer
+podman start --name hub-db
+podman start --name hub-adminer
